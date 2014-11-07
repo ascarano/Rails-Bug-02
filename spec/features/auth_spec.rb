b421157bad9ed9c088f46171db5709d26bfbd33c
ascarano <adamscarano@gmail.com>
2014-11-07 11:47:49 -0700

    completed bug, needed -id in registration controller

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -4,7 +4,7 @@ require 'capybara/rails'
 feature 'Auth' do
 
   scenario 'Users can login and out' do
-    create_user email: "user@example.com"
+    create_user email: "user@example.com", password:"password"
 
     visit root_path
     within(".signin-form") { click_on "Login" }
@@ -13,7 +13,23 @@ feature 'Auth' do
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     within(".signin-form") { click_on "Login" }
-    expect(page).to have_content("user@example.com")
+    expect(page).to have_content("Welcome user@example.com")
+    click_on "Logout"
+    expect(page).to have_content("Login")
+    click_on "Register"
+    fill_in "Name", with: "Adam"
+    fill_in "Email", with: "adam@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    within(".registration-form") do
+      click_on("Register")
+    end
+    expect(page).to have_content("Welcome adam@example.com")
+    click_on "Logout"
+    expect(page).to have_content("Login")
+    
+
+
   end
 
 end
